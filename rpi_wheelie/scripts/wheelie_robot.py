0b365d3850a5bc4c6185336be651741510822b39
marloncori <84450525+marloncori@users.noreply.github.com>
2021-08-26 00:02:48 +0200

    Update wheelie_robot.py

--- rpi_wheelie/scripts/wheelie_robot.py
+++ rpi_wheelie/scripts/wheelie_robot.py
@@ -1,37 +1,41 @@
 from motor_driver import MotorDriver
-import RPi.GPIO as gpio
 from time import sleep
 
 class Wheelie:
     def __init__(self):
-        self.rightWheel = MotorDriver(10, 9)
-        self.leftWheel = MotorDriver(8, 7)
+        self.rightWheel = MotorDriver('d:10:p','d:12:p', 'd:9:o')
+        self.leftWheel = MotorDriver('d:8:p', 'd:7:p', 'd:6:o')
 
     def stop(self):
         self.leftWheel.stop()
         self.rightWheel.stop()
 
-    def goForward(self, speed=100)
+    def goForward(self, speed=80)
         self.rightWheel.forward(speed)
         self.leftWheel.forward(speed)
 
-    def goBackward(self, speed=100)
+    def goBackward(self, speed=80)
         self.rightWheel.reverse(speed)
         self.leftWheel.reverse(speed)
 
-    def goLeft(self, speed=100)
+    def goLeft(self, speed=80)
         self.rightWheel.reverse(speed)
         self.leftWheel.forward(speed)
         
-    def goRight(self, speed=100)
+    def goRight(self, speed=80)
         self.rightWheel.forward(speed)
         self.leftWheel.reverse(speed)
+        
+    def shutdown(self):
+        self.rightWheel.finish()
+        self.leftWheel.finish()
 
+robotcar = Wheelie()
 def run():
-    robotcar = Wheelie()
+
     robotcar.goForward(70)
     sleep(1)
-    robotcar.goRight()
+    robotcar.goRight(60)
     sleep(1)
     robotcar.goBackward(50)
     sleep(1)
@@ -46,4 +50,4 @@ if __name__ == '__main__':
             run()
     except KeyboardInterrupt:
         print("Master has stopped the robot car")
-        gpio.cleanup()
+        robotcar.shutdown()
