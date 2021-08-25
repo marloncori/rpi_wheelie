65124df0b31d842e1abae27b5e1cd19e2bf1e0af
marloncori <84450525+marloncori@users.noreply.github.com>
2021-08-25 07:57:08 +0200

    Update wheelie_min_subscriber.py

--- rpi_wheelie/scripts/wheelie_min_subscriber.py
+++ rpi_wheelie/scripts/wheelie_min_subscriber.py
@@ -1,52 +1,41 @@
 #! /usr/bin/env python
 
 from wheelie_robot import Wheelie
-import rclpy
-from rclpy.node import Node
+import rospy
 from std_msgs.msg import String
-import RPi.GPIO as gpio
 import time
 
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
+robot_car = Wheelie()
 
-class MinimalSubscriber(Node):
-    def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(String,
-                                                     'move', sef.listener_callback, 10)
-        self.subscription   arning
-        self.robot_car = Wheelie()
+def listener_callback(self, msg):
+    command = msg.data
+    if command == 'forward' or command == 'forwards':
+        print("Moving forward")
+        robot_car.goForward()
+    elif command == 'backward' or command == 'reverse':
+        print("Moving backward")
+        robot_car.goBackward()
+    elif command == 'right':
+        print("Turning right")
+        robot_car.goRight()
+    elif command == 'left':
+        print("Turning left")
+        robot_car.goLeft()
+    else:
+        print("Unkown command. The robot car has stopped")
+        robot_car.stop()
+        
+def minimal_subscriber:
+    rospy.init_node('robot_whelie', anonymous=True)
+    rospy.Subscribe('move', String, listener_callback, queue_size=10)
+    rospy.spin()
 
-    def listener_callback(self, msg):
-        command = msg.data
-        if command == 'forward' or command == 'forwards':
-            print("Moving forward")
-            self.robot_car.goForward()
-        elif command == 'backward' or command == 'reverse':
-            print("Moving backward")
-            self.robot_car.goBackward()
-        elif command == 'right':
-            print("Turning right")
-            self.robot_car.goRight()
-        elif command == 'left':
-            print("Turning left")
-            self.robot_car.goLeft()
-        else:
-            print("Unkown command. The robot car has stopped")
-            self.robot_car.stop()
-
-def main(args=None):
-    rclpy.init(args=args)
-    mininal_subscriber = MinimalSubscriber()
-    rclpy.spin(minimal_subscriber)
-
-    minimal_subscriber.robot_car.stop()
-    gpio.cleanup()
-    minimal_subscriber.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+def shutdown():
+    robot_car.end()
     
-  
+if __name__ == '__main__':
+    try:
+        minimal_subscriber()
+    except rospy.ROSInterrupt:
+        rospy.loginfo("User has finished program execution.")
+        shutdown()
