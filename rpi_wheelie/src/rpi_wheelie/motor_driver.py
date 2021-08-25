f89f1ab6fb36b5067597273b58d005c00c84c272
marloncori <84450525+marloncori@users.noreply.github.com>
2021-08-25 23:54:36 +0200

    Update motor_driver.py

--- rpi_wheelie/src/rpi_wheelie/motor_driver.py
+++ rpi_wheelie/src/rpi_wheelie/motor_driver.py
@@ -1,22 +1,34 @@
-#! /usr/bin/env python
-import RPi.GPIO as rpi
-import time
+#! /usr/bin/env python3
+from pyfirmata import Arduino, util
+from time import sleep
 
-rpi.setmode(rpi.BCM)
-rpi.setwarnings(False)
+class MotorDriver(Arduino):
 
-class MotorDriver:
-
-    def __init__(self, pinFwd, pinBkw, frequency=20, maxSpeed=100):
-        rpi.setup(pinFwd, rpi.OUT)
-        rpi.setup(pinBack, rpi.OUT)
+    def __init__(self, fwd_pin, bck_pin, frequency=20, minSpeed=0, maxSpeed=100):
+        
+        self._positive = fwd_pin
+        self._negative = bck_pin
+        self._enable = en_pin
 
         self._frequency = frequency
+        self._minSpeed = minSpeed
         self._maxSpeed = maxSpeed
-        self._pwmFwd = rpi.PWM(pinFwd, frequency)
-        self._pwmBkw = rpi.PWM(pinBkw, frequency)
-        self.stop()
-
+        self._pwm = None
+        
+        self.uno = Arduino('/dev/ttyACM0')
+        self.it = util.Iterator(self.uno)
+        self.pinF = self.uno.get_pin(self._positive)
+        self.pinB = self.uno.get_pin(self._negative)
+        self.pinE = self.uno.get_pin(self._enable)
+ 
+    def _speed_map(self, val, i_min, i_max, o_min, o_max):
+        self._pwm = round(o_min + (o_max - o_min) * ((val - i_min) / (i_max - i_min)))
+        return self._pwm
+    
+    def _speed_map_abs(self, val, i_min, i_max, o_min, o_max):
+        self._pwm = abs(round(o_min + (o_max - o_min) * ((val - i_min) / (i_max - i_min))))
+        return self._pwm
+    
     def forward(self, speed):
         self._move(speed)
 
@@ -26,6 +38,10 @@ class MotorDriver:
     def stop(self):
         self._move(0)
 
+    def finish(self):
+        self._move(0)
+        self.uno.exit()
+        
     def _move(self, speed):
         #limits
         if speed > self._maxSpeed:
@@ -34,10 +50,16 @@ class MotorDriver:
             speed = -self._maxSpeed
 
         if speed < 0:
-            self._pwmFwd.start(0) #gpio pwm function
-            self._pwmBkw.start(-speed)
+            self._speed_map_abs(speed, -self._maxSpeed, self._minSpeed, 0, 256)
+            for spin in range(self._minSpeed, self._pwm, self._frequency):
+                self.pinF.write(0)
+                self.pinB.write(self._pwm)
+                self.pinE.write(1)
+                sleep(0.03)
         else:
-            self._pwmFwd.start(speed)
-            self._pwmBkw.start(0)
-
-        
+            self._speed_map(speed, self._minSpeed, self._maxSpeed, 0, 256)
+            for spin in range(self._minSpeed, self._pwm, self._frequency):
+                self.pinF.write(self._pwm)
+                self.pinB.write(0)
+                self.pinE.write(1)
+                sleep(0.03)
